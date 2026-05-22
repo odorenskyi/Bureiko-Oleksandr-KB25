6ba5238117842fff12a9fa0f6c6f5e878f73e79e
OleksandrBureiko <bureikoolhr@kntu.kr.ua>
2026-05-23 02:23:56 +0300

    ЛР11: Завершено розробку і тестування програми

--- lab11/prj/prj_4_Bureiko/main.cpp
+++ lab11/prj/prj_4_Bureiko/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <windows.h>
 
+// підключаємо спільну структуру та всі три бібліотеки команди
 #include "struct_type_project_4.h"
 #include "Modules_Bureiko.h"
+#include "Modules_Rudenko.h"
+#include "Modules_Primak.h"
 
 using namespace std;
 
@@ -14,17 +17,22 @@ int main() {
     // ініціалізуємо порожній список
     RouteNode* head = nullptr;
 
+    // назва файлу бази даних
+    const char* db_filename = "routes_db.dat";
+
     cout << "================ ІНІЦІАЛІЗАЦІЯ СИСТЕМИ ================\n";
-    cout << ">>> Тестовий режим \n";
+    // функція 3: автоматичне завантаження БД при запуску
+    load_from_binary(head, db_filename);
     cout << "=======================================================\n";
 
     char choice;
     while (true) {
-        cout << "\n===== МЕНЮ ТЕСТУВАННЯ =====\n";
-        cout << "1 - Додавання нового рейса (Функція 1)\n";
-        cout << "2 - Вилучення рейса за номером (Функція 2)\n";
-        cout << "3 - [DEBUG] Показати поточні рейси у пам'яті\n";
-        cout << "0 - Завершення роботи\n";
+        cout << "\n===== ГОЛОВНЕ МЕНЮ (ІС \"Розклад руху автобусів\") =====\n";
+        cout << "1 - Пошук запису за назвою автостанції\n";
+        cout << "2 - Виведення всього розкладу руху\n";
+        cout << "3 - Додавання нового рейса\n";
+        cout << "4 - Вилучення рейса за номером\n";
+        cout << "0 - Завершення роботи та збереження бази даних\n";
         cout << "Оберіть дію: ";
         cin >> choice;
 
@@ -34,31 +42,31 @@ int main() {
 
         switch (choice) {
             case '1': {
-                // функція 1: додавання
-                add_route(head);
+                char station[100];
+                cout << "Введіть назву автостанції (початкової або кінцевої): ";
+                cin >> ws; // Очищення буфера
+                cin.getline(station, 100);
+                // функція 5
+                search_route(head, station);
                 break;
             }
             case '2': {
+                // функція 6
+                print_schedule(head);
+                break;
+            }
+            case '3': {
+                // функція 1
+                add_route(head);
+                break;
+            }
+            case '4': {
                 char route_num[15];
                 cout << "Введіть номер рейсу для вилучення: ";
                 cin >> ws;
                 cin.getline(route_num, 15);
-                // функція 2: вилучення
+                // функція 2
                 delete_route(head, route_num);
-                break;
-            }
-            case '3': {
-                // тимчасовий локальний вивід для перевірки
-                cout << "\n--- ПОТОЧНИЙ СПИСОК РЕЙСІВ ---\n";
-                RouteNode* temp = head;
-                if (temp == nullptr) {
-                    cout << "Список порожній.\n";
-                }
-                while (temp != nullptr) {
-                    cout << "Рейс №" << temp->route_number << " (" << temp->route_name << ")\n";
-                    temp = temp->next;
-                }
-                cout << "------------------------------\n";
                 break;
             }
             default: {
@@ -70,15 +78,8 @@ int main() {
     }
 
     cout << "\n================ ЗАВЕРШЕННЯ РОБОТИ ================\n";
-    cout << ">>> Збереження у файл відключено.\n";
-
-    // ручне очищення динамічної пам'яті перед виходом
-    while (head != nullptr) {
-        RouteNode* temp = head;
-        head = head->next;
-        delete temp;
-    }
-    cout << ">>> Динамічну пам'ять успішно очищено.\n";
+    // функція 4: автоматичне збереження та очищення пам'яті
+    save_to_binary(head, db_filename);
     cout << "===================================================\n";
 
     return 0;
